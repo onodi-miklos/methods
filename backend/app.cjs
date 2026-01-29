cc05ae13cffd41cbacec11282a8a85bd7996b066
Ónodi Miklós <mikonodi33@gmail.com>
2026-01-29 19:51:42 +0100

    Formatted code.

--- backend/app.cjs
+++ backend/app.cjs
@@ -1,93 +1,105 @@
-const express = require('express')
-const path = require('path')
-let {people} = require('./data.cjs')
-const {logger} = require('./logger.cjs')
-const app = express()
+const express = require("express");
+const path = require("path");
+let { people } = require("./data.cjs");
+const { logger } = require("./logger.cjs");
+const app = express();
 
-app.use([logger, express.json(), express.urlencoded({ extended: true }), express.static(path.join(__dirname, './public'))])
+app.use([
+  logger,
+  express.json(),
+  express.urlencoded({ extended: true }),
+  express.static(path.join(__dirname, "./public")),
+]);
 
-
-
-
-app.get('/data', (req, res) => {
-  let {search, limit} = req.query
-  let sortedPeople = [...people]
-  search = search?.trim().toLowerCase()
-  if(search) {
+app.get("/data", (req, res) => {
+  let { search, limit } = req.query;
+  let sortedPeople = [...people];
+  search = search?.trim().toLowerCase();
+  if (search) {
     sortedPeople = sortedPeople.filter((person) => {
-      return person.name.startsWith(search)
-    })
+      return person.name.startsWith(search);
+    });
   }
-  if(limit) {
-    sortedPeople = sortedPeople.slice(0, Number(limit))
+  if (limit) {
+    sortedPeople = sortedPeople.slice(0, Number(limit));
   }
   if (sortedPeople.length < 1) {
-    return res.status(200).json({ success: true, data: [] })
+    return res.status(200).json({ success: true, data: [] });
   }
-  res.status(200).json({success: true, data: sortedPeople})
-})
-app.get('/data/:personId', (req, res) => {
-  const {personId} = req.params
-  const person = people.find(
-    (person) => person.id === Number(personId)
-  )
-  if(!person){
-    return res.status(404).json({ success: false, msg: `no person found with id ${personId}` })
+  res.status(200).json({ success: true, data: sortedPeople });
+});
+app.get("/data/:personId", (req, res) => {
+  const { personId } = req.params;
+  const person = people.find((person) => person.id === Number(personId));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person found with id ${personId}` });
   }
-  
-  return res.status(200).json({success: true, data: person})
-})
 
-app.post('/data', (req, res) => {
-  const { name } = req.body
-  
-  const ids = people.map((e) => {return e.id})
-  let newId
-  for(let i = 1;i <= ids.length+1; i++){
-    if(!ids.includes(i)){
-      newId = i
-      break
-    }}
-    let newPerson = { id: newId, name }
-    
-    if (!name) {
-      return res.status(400).json({success: false, msg: 'please provide name'})
+  return res.status(200).json({ success: true, data: person });
+});
+
+app.post("/data", (req, res) => {
+  const { name } = req.body;
+
+  const ids = people.map((e) => {
+    return e.id;
+  });
+  let newId;
+  for (let i = 1; i <= ids.length + 1; i++) {
+    if (!ids.includes(i)) {
+      newId = i;
+      break;
     }
-  people.push(newPerson)
+  }
+  let newPerson = { id: newId, name };
+
+  if (!name) {
+    return res.status(400).json({ success: false, msg: "please provide name" });
+  }
+  people.push(newPerson);
   res.status(201).json({
     success: true,
-    data: people
-  })
-})
+    data: people,
+  });
+});
 
-app.put('/data/:personId', (req, res) => {
-  const {personId} = req.params
-  const {name} = req.body
+app.put("/data/:personId", (req, res) => {
+  const { personId } = req.params;
+  const { name } = req.body;
 
-  const isPerson = people.find((person) => person.id === Number(personId))
-  if(!isPerson){
-    return res.status(404).json({success: false, msg: `no person found with id ${personId}`})
+  const isPerson = people.find((person) => person.id === Number(personId));
+  if (!isPerson) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person found with id ${personId}` });
   }
   people.forEach((person) => {
     if (person.id === Number(personId)) {
-      person.name = name
+      person.name = name;
     }
-    return person
-  })
+    return person;
+  });
 
-  res.status(200).json({success: true, data: people})
-})
+  res.status(200).json({ success: true, data: people });
+});
 
-app.delete('/data/:personId', (req, res) => {
-  const person = people.find((person) => person.id === Number(req.params.personId))
-  if(!person){
-    return res.status(404).json({success: false, msg: `no person found with id ${req.params.personId}`})
+app.delete("/data/:personId", (req, res) => {
+  const person = people.find(
+    (person) => person.id === Number(req.params.personId),
+  );
+  if (!person) {
+    return res.status(404).json({
+      success: false,
+      msg: `no person found with id ${req.params.personId}`,
+    });
   }
 
-  people = people.filter((person) => person.id !== Number(req.params.personId))
-  return res.status(200).json({success: true, data: people})
-})
+  people = people.filter((person) => person.id !== Number(req.params.personId));
+  return res.status(200).json({ success: true, data: people });
+});
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
+  console.log("Server is listening on port 5000....");
+});
--- backend/logger.cjs
+++ backend/logger.cjs
@@ -1,13 +1,12 @@
-const { log } = require('console')
-const fs = require('fs')
-const path = require('path')
+const { log } = require("console");
+const fs = require("fs");
+const path = require("path");
 
-const logFile = path.join(__dirname, 'log.txt')
+const logFile = path.join(__dirname, "log.txt");
 
 function logger(req, res, next) {
-
-  res.on('finish', () => {
-    const timestamp = new Date().toISOString()
+  res.on("finish", () => {
+    const timestamp = new Date().toISOString();
 
     const logData = {
       method: req.method,
@@ -15,20 +14,19 @@ function logger(req, res, next) {
       status: res.statusCode,
       params: req.params,
       query: req.query,
-      body: req.body
-    }
+      body: req.body,
+    };
 
-    const logLine = `[${timestamp}] ${JSON.stringify(logData)}\n\n`
+    const logLine = `[${timestamp}] ${JSON.stringify(logData)}\n\n`;
 
     // console.log(logLine.trim())
 
-    fs.appendFile(logFile, logLine, err => {
-      if (err) console.error(err)
-    })
-  })
+    fs.appendFile(logFile, logLine, (err) => {
+      if (err) console.error(err);
+    });
+  });
 
-  next()
+  next();
 }
 
-
-module.exports = { logger }
+module.exports = { logger };
