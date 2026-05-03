4043b1378dbe6c3a9e72313a0809cbea9451c7de
Ónodi Miklós <mikonodi33@gmail.com>
2026-05-03 10:09:45 +0200

    Implemented db

--- api/app.cjs
+++ api/app.cjs
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const logger = require('@onodi-miklos/logger')
-const api = require('./routes/data.cjs')
+const router = require('./routes/data.cjs')
 
 const app = express()
 
@@ -17,13 +17,39 @@ app.use([
 ]);
 
 
-app.use("/api", api)
+app.use("/api", router)
+
+
+// db
+let db
+
+const { connectToDb, getDb } = require('./db.cjs')
+
+connectToDb((err) => {
+  if(!err) {
+    app.listen(5000, () => {
+      console.log('Server is listening on port 5000....')
+    })
+    db = getDb()
+  }
+})
+
+app.get('/db', (req, res) => {
+  let books = []
+
+  db.collection('books')
+    .find()
+    .sort({ author: 1 })
+    .forEach(book => books.push(book))
+    .then(() => {
+      res.status(200).json(books)
+    })
+    .catch(() => {
+      res.status(500).json({error: 'Could not fetch the documents'})
+    })
+})
 
 
 app.all(/.*/, (req, res) => {
   return res.status(404).send({success: false})
-})
-
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
 })
